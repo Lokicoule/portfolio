4d01253ae4cdf12f4e6ab20bb084865d2919ddea
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-08-10 13:56:49 +0200

    feat: snapshot front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import "./App.css";
+import FrontPage from "./pages/frontPage/FrontPage";
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  console.log("dark");
+  document.documentElement.classList.add("dark");
+} else {
+  console.log("light");
+  document.documentElement.classList.remove("dark");
+}
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <FrontPage />;
 }
