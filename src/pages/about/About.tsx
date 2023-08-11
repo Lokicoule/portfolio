ffd164c6f82f0ad58a40a15c7d51e1a51ba0620b
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-08-11 15:25:44 +0200

    feat: init pages

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+    </div>
+  );
+};
+
+export default About;
