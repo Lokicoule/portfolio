9fba2b3029ff46d692bb1869e8deb742cef07741
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-08-11 18:09:49 +0200

    feat: app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
 import "./App.css";
 import FrontPage from "./pages/frontPage/FrontPage";
 
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  console.log("dark");
-  document.documentElement.classList.add("dark");
-} else {
-  console.log("light");
-  document.documentElement.classList.remove("dark");
-}
-
 export default function App() {
   return <FrontPage />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppShell from "./shared/components/AppShell/AppShell.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppShell />
+  </React.StrictMode>
+);
