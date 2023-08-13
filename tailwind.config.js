b4d545e0b8fb93d92bceace638a13f5dc3b1481d
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-08-13 10:46:46 +0200

    chore: config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "992px",
+        xl: "1280px",
+      },
+    },
     extend: {},
   },
   plugins: [],
