6f8fa35d41a7b3fce2dec1536ea1ee8315f32da0
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-08-13 11:18:35 +0200

    feat: add theme provider to app shell

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,9 +1,14 @@
 import { HelmetProvider } from "react-helmet-async";
 import { routingService } from "../shared/composition";
 import routes from "./routing";
+import { ThemeProvider } from "../shared/persistence/theme";
 
 const AppShell = () => {
-  return <HelmetProvider>{routingService.createRoutes(routes)}</HelmetProvider>;
+  return (
+    <HelmetProvider>
+      <ThemeProvider>{routingService.createRoutes(routes)}</ThemeProvider>
+    </HelmetProvider>
+  );
 };
 
 export default AppShell;
