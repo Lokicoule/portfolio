0afbe81c166a7437db30cbcb579e46fd39403e53
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-13 22:49:04 +0200

    style: reverse gradient on dark screen

--- src/shared/components/layouts/FlexLayout.tsx
+++ src/shared/components/layouts/FlexLayout.tsx
@@ -2,7 +2,7 @@ import Header from "../header/Header";
 
 const FlexLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-sky-to-grey h-screen w-full flex flex-col justify-center items-center min-h-screen antialiased">
+    <div className="bg-sky-to-grey dark:bg-grey-to-sky h-screen w-full flex flex-col justify-center items-center min-h-screen antialiased">
       <Header />
       <main className="flex-grow flex flex-col justify-center items-center">
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
