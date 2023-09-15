377295f17701078543d0934e3f2d83fd4bcb0d95
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-15 16:41:37 +0200

    style(layout): remove margin top

--- src/shared/components/layouts/PageLayout.tsx
+++ src/shared/components/layouts/PageLayout.tsx
@@ -18,7 +18,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children, title }) => {
         animate="visible"
         exit="hidden"
       >
-        <h2 className="pt-12 px-2 sm:px-5 md:px-10 lg:px-14 page-title mt-12 lg:mt-0 mb-12 md:mb-[30px]">
+        <h2 className="pt-12 px-2 sm:px-5 md:px-10 lg:px-14 page-title mb-12 md:mb-[30px]">
           {title}
         </h2>
         {children}
