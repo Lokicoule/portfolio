36a86f23f5b900f5f3cdc1135c2e0859b32fb0e7
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-15 22:17:28 +0200

    chore: remove unused file

--- src/shared/composition/setupRepositories.ts
+++
@@ -1,9 +0,0 @@
-/* import { translatingService } from ".";
-import { ExperienceRepository } from "../../modules/experiences/experienceRepository";
-
-const experienceRepository = new ExperienceRepository(
-  translatingService.language
-);
-
-export { experienceRepository };
- */
