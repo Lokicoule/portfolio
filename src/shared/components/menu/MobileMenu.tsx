193272c635c067b4250176de29347ba01c5d19f0
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-16 10:29:22 +0200

    refactor(menu): use headlessui for mobile

--- src/shared/components/menu/MobileMenu.tsx
+++ src/shared/components/menu/MobileMenu.tsx
@@ -1,42 +1,56 @@
-import { useState } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import Navigation from "../navigation/Navigation";
 import { navigationPresenter } from "../../composition";
+import Navigation from "../navigation/Navigation";
+import { motion } from "framer-motion";
 
 const MobileMenu: React.FC = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setOpen((prevState) => !prevState);
-  };
-
   return (
-    <>
-      <button
-        className="lg:opacity-0 lg:hidden visible opacity-100  bg-sky-to-blue w-[40px] h-[40px] rounded-full flex justify-center cursor-pointer items-center text-white dark:text-white text-3xl ml-3 p-3"
-        onClick={toggleMenu}
-      >
-        {open ? <AiOutlineClose /> : <AiOutlineMenu />}
-      </button>
-
-      <nav className={`${open ? "block lg:hidden" : "hidden"}`}>
-        <ul
-          className={`${
-            open
-              ? "block rounded-b-[20px] shadow-md absolute left-0 top-20 z-50 w-full backdrop-blur-xl bg-white/30"
-              : "flex my-12 "
-          }`}
-        >
-          <Navigation
-            presenter={navigationPresenter}
-            defaultClassName="mobile-link-menu text-black dark:text-white"
-            activeClassName="mobile-link-menu text-white linked bg-sky-to-blue"
-            iconClassName="text-xl mr-2"
-            onClick={toggleMenu}
-          />
-        </ul>
-      </nav>
-    </>
+    <Popover>
+      {({ open }) => (
+        <>
+          <Popover.Button className="lg:opacity-0 lg:hidden visible opacity-100  bg-sky-to-blue w-[40px] h-[40px] rounded-full flex justify-center cursor-pointer items-center text-white dark:text-white text-xl font-bold">
+            <motion.div
+              animate={{
+                rotate: open ? 90 : 0,
+                scale: open ? 1.2 : 1,
+              }}
+              transition={{ duration: 0.3 }}
+            >
+              {open ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </motion.div>
+          </Popover.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200 delay-150"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-200 delay-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel
+              static
+              className={`${
+                open
+                  ? "block rounded-b-[20px] shadow-md absolute left-0 top-20 z-50 w-full bg-white dark:bg-[#05151e]"
+                  : "hidden"
+              } lg:hidden`}
+            >
+              <ul className="flex flex-col">
+                <Navigation
+                  presenter={navigationPresenter}
+                  defaultClassName="mobile-link-menu text-black dark:text-white"
+                  activeClassName="mobile-link-menu text-white linked bg-sky-to-blue"
+                  iconClassName="text-xl mr-2"
+                />
+              </ul>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
   );
 };
 
