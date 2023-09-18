b9d38011bd8db3054daab5938fb64abf10b2b846
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-18 10:37:47 +0200

    refactor(layout): move footer and update animation

--- src/shared/components/layouts/Layout.tsx
+++ src/shared/components/layouts/Layout.tsx
@@ -1,21 +1,21 @@
-import { PropsWithChildren } from "react";
-import Footer from "../footer/Footer";
-import Hero from "../hero/Hero";
-import Header from "../header/Header";
-import DesktopMenu from "../menu/DesktopMenu";
 import { AnimatePresence, motion } from "framer-motion";
+import { PropsWithChildren } from "react";
+import Header from "../header/Header";
+import Hero from "../hero/Hero";
+import DesktopMenu from "../menu/DesktopMenu";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="dark:bg-gradient-to-t from-[#FCDAB7] via-[#1E5F74] via-[#133B5C] to-[#1D2D50] bg-no-repeat bg-fixed h-full">
       <Header />
-      <main className="container grid grid-cols-12 md:gap-10 lg:mt-[180px] ">
-        <div className="col-span-12 lg:col-span-4 lg:h-screen lg:sticky top-44 hidden lg:block">
-          <AnimatePresence>
+      <AnimatePresence mode="wait">
+        <main className="container grid grid-cols-12 md:gap-10 lg:mt-[180px] ">
+          <div className="col-span-12 lg:col-span-4 lg:h-screen lg:sticky top-44 hidden lg:block">
             <motion.div
               key="hero"
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
               transition={{
                 duration: 0.3,
                 ease: [0, 0.71, 0.2, 1.01],
@@ -29,33 +29,30 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
             >
               <Hero />
             </motion.div>
-          </AnimatePresence>
-        </div>
-        <div className="col-span-12 lg:col-span-8  ">
-          <AnimatePresence>
+          </div>
+          <div className="col-span-12 lg:col-span-8  ">
             <motion.div
               key="menu"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.4 }}
+              exit={{ opacity: 0 }}
             >
               <DesktopMenu />
             </motion.div>
-          </AnimatePresence>
-          <AnimatePresence>
             <motion.div
               key="content"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               transition={{ duration: 0.5, delay: 0.9 }}
               className="lg:rounded-2xl bg-primary-light dark:bg-primary-dark"
             >
               {children}
-              <Footer />
             </motion.div>
-          </AnimatePresence>
-        </div>
-      </main>
+          </div>
+        </main>
+      </AnimatePresence>
     </div>
   );
 };
--- src/shared/components/layouts/PageLayout.tsx
+++ src/shared/components/layouts/PageLayout.tsx
@@ -1,28 +1,42 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import PageTitle, { PageTitleProps } from "../page-title/PageTitle";
+import Footer from "../footer/Footer";
 
 interface PageLayoutProps extends React.PropsWithChildren<PageTitleProps> {}
 
-const PageLayout: React.FC<PageLayoutProps> = ({ children, title }) => {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 0.75 } },
-  };
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.75 } },
+};
 
+const PageLayout: React.FC<PageLayoutProps> = ({ children, title }) => {
   return (
     <>
       <PageTitle title={title} />
-      <motion.section
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-      >
-        <h2 className="text-5xl font-bold dark:text-white relative inline-block pt-12 px-2 sm:px-5 md:px-10 lg:px-14 mb-12 md:mb-[30px]">
-          {title}
-        </h2>
-        {children}
-      </motion.section>
+      <AnimatePresence mode="wait">
+        <motion.section
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <h2 className="text-5xl font-bold dark:text-white relative inline-block pt-12 px-2 sm:px-5 md:px-10 lg:px-14 mb-12 md:mb-[30px]">
+            {title}
+          </h2>
+          {children}
+        </motion.section>
+
+        <motion.div
+          key="footer"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          className="lg:rounded-2xl bg-primary-light dark:bg-primary-dark"
+        >
+          <Footer />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
