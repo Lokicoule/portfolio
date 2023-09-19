0a1e2e0bb7b7b2682fc636554cdf7055021a6c5a
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-09-19 23:04:49 +0200

    style(footer): update hover color

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <p className="text-center py-6 text-secondary">
         © 2023 All Rights Reserved by{" "}
         <a
-          className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-500 inline-block text-transparent bg-clip-text font-bold hover:text-white"
+          className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-500 inline-block text-transparent bg-clip-text font-bold hover:text-primary"
           href="#"
           target="_blank"
           rel="noopener noreferrer"
